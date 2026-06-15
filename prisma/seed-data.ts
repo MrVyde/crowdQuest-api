79f3fe4eb40d8010c8a5296c55270e184b7d8bfb
MrVyde <Mrvyde@outlook.com>
2026-06-15 14:01:55 +0100

    Add seed data for game images and character coordinate definitions

---
+++ prisma/seed-data.ts
@@ -0,0 +1,40 @@
+export const gameImages = [
+  {
+    title: "CrowdQuest City",
+    imageUrl: "/dashImage/crowd2.webp",
+
+    characters: [
+      {
+        name: "Ted",
+        thumbnailUrl: "/characters/owen1.webp",
+
+        minX: 0.390,
+        maxX: 0.409,
+
+        minY: 0.371,
+        maxY: 0.395,
+      },
+
+      {
+        name: "Amika",
+        thumbnailUrl: "/characters/owen2.jpg",
+
+        minX: 0.5418,
+        maxX: 0.5562,
+        minY: 0.5304,
+        maxY: 0.5557,
+      },
+
+      {
+        name: "Ned",
+        thumbnailUrl: "/characters/owen3.jpg",
+
+        minX: 0.4426,
+        maxX: 0.4598,
+        minY: 0.4306,
+        maxY: 0.4557,
+        
+      },
+    ],
+  },
+];
